aa51fcdbc29ab874e00a492f56c72bf8ffea54a6
sivalgabe <sival.gabriel@gmail.com>
2022-09-02 16:24:00 -0300

    adicionadas funçoes aos botÃões

--- app/src/main/java/br/com/igti/ciclodevida/MainActivity.kt
+++ app/src/main/java/br/com/igti/ciclodevida/MainActivity.kt
@@ -1,11 +1,26 @@
 package br.com.igti.ciclodevida
 
+import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun configListeners(){
+        configListenersBtnResultado()
+    }
+
+    private fun configListenersBtnResultado() {
+        val button =findViewById<Button>(R.id.btnMainAction)
+        button.setOnClickListener {
+            val intent = Intent(this, ResultadoActivity)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/br/com/igti/ciclodevida/ResultadoActivity.kt
+++ app/src/main/java/br/com/igti/ciclodevida/ResultadoActivity.kt
@@ -2,10 +2,18 @@ package br.com.igti.ciclodevida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+        acaoBotaoVoltar()
+    }
+
+    private fun acaoBotaoVoltar() {
+                val button = findViewById<Button>(R.id.btnResultadoActivity)
+        button.setOnClickListener { finish()
+    }
     }
 }
