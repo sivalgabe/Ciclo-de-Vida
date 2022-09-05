56fa977d992e5c8fad54efb031d32c0e5d45c392
sivalgabe <sival.gabriel@gmail.com>
2022-09-05 10:44:35 -0300

    adicionado o segundo botão na segunda activity

--- app/src/main/java/br/com/igti/ciclodevida/ResultadoActivity.kt
+++ app/src/main/java/br/com/igti/ciclodevida/ResultadoActivity.kt
@@ -2,17 +2,32 @@ package br.com.igti.ciclodevida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        button = findViewById<Button>(R.id.btnResultadoActivity)
         setContentView(R.layout.activity_resultado)
+        configListeners()
         acaoBotaoVoltar()
     }
 
+    private fun configListeners() {
+        acaoBotaoVoltar()
+        acaoBotaoNovo()
+    }
+
+    private fun acaoBotaoNovo() {
+        findViewById<Button>(R.id.btnNovo).setOnClickListener {
+            button.visibility = View.VISIBLE
+        }
+    }
+
+
     private fun acaoBotaoVoltar() {
-                val button = findViewById<Button>(R.id.btnResultadoActivity)
         button.setOnClickListener {
             finish()
     }
